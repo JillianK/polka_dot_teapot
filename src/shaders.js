73a4dedc86028c18d4dbdd90cd7b1025eb5c0d2b
Eric Y <eye@usc.edu>
2021-04-14 16:22:25 -0700

    Spread function calls

--- src/shaders.js
+++ src/shaders.js
@@ -4,9 +4,9 @@
  * @param {Triangle} tri
  */
  function phongShader(tri) {
-    const pos0 = tri[0].slice(0,3);
-    const pos1 = tri[1].slice(0,3);
-    const pos2 = tri[2].slice(0,3);
+    const [xi0,yi0,zi0] = tri[0].slice(0,3);
+    const [xi1,yi1,zi1] = tri[1].slice(0,3);
+    const [xi2,yi2,zi2] = tri[2].slice(0,3);
     const [x0_, y0_, z0, nx0, ny0, nz0,,,,u0,v0] = transformPosition(...tri[0]);
     const [x1_, y1_, z1, nx1, ny1, nz1,,,,u1,v1] = transformPosition(...tri[1]);
     const [x2_, y2_, z2, nx2, ny2, nz2,,,,u2,v2] = transformPosition(...tri[2]);
@@ -47,17 +47,21 @@
             if (alpha >= 0 && beta >= 0 && gamma >= 0) {
                 let zPix = zAtPix(alpha, beta, gamma);
                 if (zPix < state.z[x + y * width]) {
-                    /**@type {Color} */
-                    let norm = vecAdd(
-                        dVMultFn(alpha,[nx0,ny0,nz0]),
-                        dVMultFn(beta,[nx1,ny1,nz1]),
-                        dVMultFn(gamma,[nx2,ny2,nz2]));
-                    norm = normalize(norm);
+                    /**@type {Color} */ // norm = alpha*n0 + beta*n1 + gamma*n2
+                    const norm = normalizeMut([
+                        alpha*nx0 + beta*nx1 + gamma*nx2,
+                        alpha*ny0 + beta*ny1 + gamma*ny2,
+                        alpha*nz0 + beta*nz1 + gamma*nz2]);
                     const [r,g,b] = state.computeColor(norm,
-                        vecAdd(
+                        /*vecAdd(
                             dVMultFn(alpha,pos0),
                             dVMultFn(beta,pos1),
-                            dVMultFn(gamma,pos2)),
+                            dVMultFn(gamma,pos2)),*/
+                        [
+                            alpha*xi0 + beta*xi1 + gamma*xi2,
+                            alpha*yi0 + beta*yi1 + gamma*yi2,
+                            alpha*zi0 + beta*zi1 + gamma*zi2,
+                        ],
                         [
                             (alpha*u0/z0+beta*u1/z1+gamma*u2/z2)*zPix,
                             (alpha*v0/z0+beta*v1/z1+gamma*v2/z2)*zPix
@@ -116,7 +120,7 @@
             if (alpha >= 0 && beta >= 0 && gamma >= 0) {
                 let zPix = zAtPix(alpha, beta, gamma);
                 if (zPix < state.z[x + y * width]) {
-                    const [r,g,b] = dVMultFn(255, getTexture(
+                    const [r,g,b] = dVMultFnMut(255, getTexture(
                             (alpha*u0+beta*u1+gamma*u2),
                             (alpha*v0+beta*v1+gamma*v2)
                         ));
@@ -138,10 +142,6 @@
     const [x0_, y0_, z0, , , , r0,g0,b0,u0,v0] = transformPosition(...tri[0]);
     const [x1_, y1_, z1, , , , r1,g1,b1,u1,v1] = transformPosition(...tri[1]);
     const [x2_, y2_, z2, , , , r2,g2,b2,u2,v2] = transformPosition(...tri[2]);
-
-    const c0 = [r0,g0,b0];
-    const c1 = [r1,g1,b1];
-    const c2 = [r2,g2,b2];
 
     const x0 = width_helper(x0_);
     const y0 = height_helper(y0_);
@@ -180,11 +180,14 @@
                 if (zPix < state.z[x + y * width]) {
                     /**@type {Color} */
                     const [r,g,b] = 
+                        
                         vecAdd(
-                            dVMultFn(alpha,c0),
-                            dVMultFn(beta, c1),
-                            dVMultFn(gamma, c2),
-                            dVMultFn(255,getTexture(
+                            [
+                                alpha*r0 + beta*g0 + gamma*b0,
+                                alpha*r1 + beta*g1 + gamma*b1,
+                                alpha*r2 + beta*g2 + gamma*b2
+                            ],
+                            dVMultFnMut(255,getTexture(
                                 (alpha*u0/z0+beta*u1/z1+gamma*u2/z2)*zPix,
                                 (alpha*v0/z0+beta*v1/z1+gamma*v2/z2)*zPix
                             ))
